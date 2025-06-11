6923405ce247bbb93b6eab7966340eac597ed8bd
SandlotWizards <staylor@sandlotwizards.com>
2025-06-11 09:55:36 -0400

    feat: introduce ValidationResult model

---
+++ src/Sandlot.ActionLogger/Models/ValidationResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Sandlot.ActionLogger.Models
+{
+    public class ValidationResult
+    {
+        public bool IsValid { get; }
+        public string? Message { get; }
+
+        private ValidationResult(bool isValid, string? message)
+        {
+            IsValid = isValid;
+            Message = message;
+        }
+
+        public static ValidationResult Success() => new(true, null);
+        public static ValidationResult Fail(string message) => new(false, message);
+    }
+}
